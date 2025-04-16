6b7e1e3055b4f2089dd1fa6723ee03d7c9ce894a
Moon Ji Hoon <variableruby@gmail.com>
2025-04-16 20:39:09 +0800

    update jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 #PBS -P volta_pilot
 #PBS -j oe
-#PBS -N phi2_run
+#PBS -N watermark_paraphrase
 #PBS -q volta_gpu
 #PBS -l select=1:ncpus=5:mem=50gb:ngpus=1:mpiprocs=1
-#PBS -l walltime=04:00:00
+#PBS -l walltime=24:00:00
 
-cd ${PBS_O_WORKDIR}
+cd $PBS_O_WORKDIR;
+np=$(cat ${PBS_NODEFILE} | wc -l);
 
-# Create a temporary scratch directory
-scratch=/scratch/${USER}/${PBS_JOBID}
-export TMPDIR=$scratch
-mkdir -p $scratch
+image="/app1/common/singularity-img/3.0.0/tensorflow_1.12_nvcr_19.01-py3.simg"
 
-# Run Python program:
-python demo/generate.py data/in/sample.txt --model microsoft/phi-2 --key 42 --output data/out/sample_paraphrased.text --verbose
+singularity exec $image bash << EOF > stdout.$PBS_JOBID 2> stderr.$PBS_JOBID
 
-# Clean up scratch space
-rm -rf $scratch
-exit $?
+# Test print
+echo "This is a test print"
+
+# you can put more commands here
+# echo “Hello World”
+EOF
