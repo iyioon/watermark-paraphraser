30b9c6605f5624b91785b14f24bf1ad3f278a546
Moon Ji Hoon <variableruby@gmail.com>
2025-04-16 20:13:59 +0800

    update jobscript

--- jobscript.sh
+++ jobscript.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
-#PBS -P myproj
+#PBS -P watermarkparaphrase
 #PBS -j oe
-#PBS -N myprog
+#PBS -N watermark/paraphrase
 #PBS -l select=1:ncpus=1
 #PBS -l place=free:shared
 
-cd ${PBS_O_WORKDIR};   ## this line is needed, do not delete.
+cd ${PBS_O_WORKDIR}
 
 # Create a temporary scratch directory
 scratch=/scratch/${USER}/${PBS_JOBID}
@@ -16,6 +15,6 @@ mkdir -p $scratch
 # Run Python program:
 python demo/generate.py data/in/sample.txt --model microsoft/phi-2 --key 42 --output data/out/sample_paraphrased.text --verbose
 
-# Clean up scratch space if
+# Clean up scratch space
 rm -rf $scratch
 exit $?
